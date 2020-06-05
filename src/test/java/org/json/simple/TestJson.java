e4ce235afe67444857efcabcc20fffb8d4f840f4
msi1 <r.sekaleshfar@gmail.com>
2020-06-06 00:21:28 +0430

    Add tests to higher the coverage of parser package

--- src/test/java/org/json/simple/TestJson.java
+++ src/test/java/org/json/simple/TestJson.java
@@ -4,15 +4,18 @@
  */
 package org.json.simple;
 
-import static org.junit.Assert.assertEquals;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.Yytoken;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestJson {
 
-	@Test	
+	@Test
 	public void testDecode() throws Exception{
 		System.out.println("=======decode=======");
 
@@ -24,14 +27,49 @@ public class TestJson {
 		System.out.println();
 		assertEquals("10",array.get(1).toString());
 	}
-	
+
 	@Test
 	public void testJSONArrayCollection() {
 		final ArrayList<String> testList = new ArrayList<String>();
 		testList.add("First item");
 		testList.add("Second item");
 		final JSONArray jsonArray = new JSONArray(testList);
-		
+
 		assertEquals("[\"First item\",\"Second item\"]", jsonArray.toJSONString());
+	}
+
+	@Test
+	public void testJSONObjectParse() {
+		try {
+			Object dictObj =  JSONValue.parseWithException("{\"anime\": [1,2,3]}");
+			Object arrayObj = JSONValue.parseWithException("[1,2,3]");
+			JSONObject jsonObject = (JSONObject) dictObj;
+			JSONArray jsonArray = (JSONArray) arrayObj;
+			assertEquals(((JSONArray) jsonObject.get("anime")).toJSONString(), jsonArray.toJSONString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testYyTokenToString() {
+		try {
+			Object object =  JSONValue.parseWithException("[{},{\"one\":\"two\"}]");
+			Yytoken token = new Yytoken(Yytoken.TYPE_VALUE, object);
+			assertEquals(token.toString(), "VALUE([{},{\"one\":\"two\"}])");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testParseException() {
+		boolean errorOccured = false;
+		try {
+			Object object = JSONValue.parseWithException("This is wrong");
+		} catch (ParseException e) {
+			errorOccured = true;
+		}
+		assertTrue(errorOccured);
 	}
 }
